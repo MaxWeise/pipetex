dac66d539aa9115bc1c7085f424b66c9fdc4a55b
MaxWeise <max.weise@student.reutlingen-university.de>
2022-07-23 17:59:44 +0200

    ÃAdded helper functions and test files to the test directory

---
+++ tests/test_helpers/util_functions.py
@@ -0,0 +1,27 @@
+""" This module contains helper functions for the test modules
+
+The functions defined in this module can be usefull in different modules
+of the test directory and are therefore outsurced in this file.
+
+@author Max Weise
+created 23.07.2022
+"""
+
+import os
+import shutil
+
+
+def remove_files(file_name: str):
+    """ Removes files containing specified name.
+
+    Args:
+        file_name: Name of the file. Function searches this substring and
+            removes the file.
+    """
+
+    list_of_files: list[str] = os.listdir(".")
+
+    for f in list_of_files:
+        if file_name in f:
+            os.remove(f)
+
