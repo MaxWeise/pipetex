688a7ae2f5779a902563021e8105764b5f7a8d62
MaxWeise <max.weise@student.reutlingen-university.de>
2022-07-27 14:54:58 +0200

    Added an Enum which holds the keys to the config_dict

--- src/pipetex/utils/enums.py
+++ src/pipetex/utils/enums.py
@@ -15,3 +15,13 @@ class SeverityLevels(enum.IntEnum):
     HIGH = 20
     CRITICAL = 30
 
+
+class ConfigDictKeys(enum.Enum)
+    """This enum holds all allowed keys for the shared config dictionary.
+
+    General Rules:
+        Add keys as needed, but only if absolutely needed.
+        string values have to be snake_case
+    """
+
+    NEW_NAME = "new_name"
