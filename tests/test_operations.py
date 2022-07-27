ad17f1183b09d92828d1fdb6a2dc782521af1f99
MaxWeise <max.weise@student.reutlingen-university.de>
2022-07-27 11:35:04 +0200

    Updated testfiles to test for the Monadic error handling

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -9,52 +9,65 @@ entirely clear right now.
 created 23.07.2022
 """
 
+from typing import Optional, Tuple
 from src.pipetex.operations import operations
+from src.pipetex.utils import enums, exceptions
+
 from tests.shared_fixtures import testfile_tex, config_dict, testfile_tex_no_draft_option   # noqa: E501
 
 import os
 import pytest
 
+
+# === Type Def ===
+Monad = Tuple[bool, Optional[exceptions.InternalException]]
+
 def test_remove_draft_option(testfile_tex, config_dict):
     """Tests that the draft option from the classdefinition gets removed. """
     file_name = testfile_tex
 
-    # run test function
-    underTest = operations.remove_draft_option(file_name, config_dict)  # noqa: F841, E501
+    underTest: Monad = operations.remove_draft_option(file_name, config_dict)
 
     with open(f"{file_name}.tex", "r") as f:
         lines_in_testfile: list[str] = [line.rstrip() for line in f]
 
-    # assert conditions
+    assert underTest[0]
+    assert not underTest[1]
     assert "draft" not in lines_in_testfile[0]
     assert len(lines_in_testfile) == 4
 
 
 def test_remove_draft_option_fileNotFound(config_dict):
     """Tests that the draft option from the classdefinition gets removed. """
-    # run test function
-    with pytest.raises(FileNotFoundError):
-        underTest = operations.remove_draft_option("not_a_file", config_dict)   # noqa: F841, E501
+    underTest: Monad = operations.remove_draft_option(
+        "not_a_file",
+        config_dict
+    )
+
+    assert not underTest[0]
+    assert type(underTest[1].severity_level) == int
+    assert 20 < underTest[1].severity_level <= 30
 
 
 def test_remove_draft_option_draftOptionNotFound(testfile_tex_no_draft_option,
                                                  config_dict):
     """Tests that the draft option from the classdefinition gets removed. """
     file_name = testfile_tex_no_draft_option
-    # Collect lines in file
+
     with open(f"{file_name}.tex", "r") as f:
         lines_befor_runing_test: list[str] = [line.rstrip() for line in f]
 
-    # run test function
-    with pytest.raises(ValueError):
-        underTest = operations.remove_draft_option(   # noqa: F841
-            testfile_tex_no_draft_option,
-            config_dict
-        )
+    underTest = operations.remove_draft_option(
+        testfile_tex_no_draft_option,
+        config_dict
+    )
 
     with open(f"{file_name}.tex", "r") as f:
         lines_after_runing_test: list[str] = [line.rstrip() for line in f]
 
     assert lines_after_runing_test == lines_befor_runing_test
+    assert not underTest[0]
+    assert type(underTest[1].severity_level) == int
+    assert underTest[1].severity_level <= 10
 
 
